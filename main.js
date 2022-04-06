1289fe04a6c4e227dddfe6ee41acb0d51ac4ab08
Mikhail Kudinov <91945835+uselessdeveloperFr@users.noreply.github.com>
2022-04-06 14:10:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -17,9 +17,7 @@ const highlightMenu = () => {
 	const aboutMenu = document.querySelector('#about-page')
 	const servicesMenu = document.querySelector('#services-page')
 	let scrollPos = window.scrollY
-	// console.log(scrollPos);
 
-	// adds 'highlight' class to my menu items
 	if (window.innerWidth > 960 && scrollPos < 600) {
 		homeMenu.classList.add('highlight')
 		aboutMenu.classList.remove('highlight')
